af37f02e8ea539c6b9fad5bd7a76832a2b560289
Duccio <ducciobreschi@gmail.com>
2020-11-16 12:49:45 +0100

    Add lit-element minimal code

--- tests/dummy.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import 'mocha';
-import dummy from '../dummy';
-
-describe('Dummy test', () => {
-  it('should return true', () => {
-    expect(dummy()).to.be.true;
-  });
-});
