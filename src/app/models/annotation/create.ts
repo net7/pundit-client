5d5767e4c627aba90b0fba4f425efd924da3e771
Edgar Gomez <gomez@netseven.it>
2020-12-11 14:56:07 +0100

    added class comment

--- src/app/models/annotation/create.ts
+++ src/app/models/annotation/create.ts
@@ -2,6 +2,10 @@ import { describe } from '../anchoring/html';
 import { AnnotationCreateData } from '.';
 import { getDocumentHref, getDocumentTitle } from './html-util';
 
+/**
+ * Creates a new annotation that is associated with the selected region of
+ * the current document.
+ */
 export function create(selection: Range, root: HTMLElement = document.body): AnnotationCreateData {
   return {
     metadata: {
