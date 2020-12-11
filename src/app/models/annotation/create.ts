817e4c23a1e58512042e6d2eeb92774101b482f4
Edgar Gomez <gomez@netseven.it>
2020-12-11 14:15:08 +0100

    added annotation create

---
+++ src/app/models/annotation/create.ts
@@ -0,0 +1,13 @@
+import { describe } from '../anchoring/html';
+import { AnnotationCreateData } from '.';
+import { getDocumentHref, getDocumentTitle } from './html-util';
+
+export function create(selection: Range, root: HTMLElement = document.body): AnnotationCreateData {
+  return {
+    metadata: {
+      href: getDocumentHref(),
+      title: getDocumentTitle()
+    },
+    selector: describe(root, selection)
+  };
+}
---
+++ src/app/models/annotation/types.ts
@@ -0,0 +1,7 @@
+export type AnnotationCreateData = {
+  metadata: {
+    href: string;
+    title: string;
+  };
+  selector: any[];
+}
