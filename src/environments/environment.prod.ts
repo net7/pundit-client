c8c8c854fa273ed0584b5f609bd1386f7fb8ad41
Marco Zizi <zizi@netseven.it>
2020-12-28 16:51:03 +0100

    update annotation-server baseURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   serializer: 'pundit-client',
-  baseURL: 'https://omaqn3375i.execute-api.eu-south-1.amazonaws.com/stage/'
+  baseURL: 'https://4b0gec68z3.execute-api.eu-south-1.amazonaws.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   serializer: 'pundit-client',
-  baseURL: 'https://omaqn3375i.execute-api.eu-south-1.amazonaws.com/stage/'
+  baseURL: 'https://4b0gec68z3.execute-api.eu-south-1.amazonaws.com/'
 };
 
 /*
