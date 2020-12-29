6c550c2e795b2e75b6ba7ca1a437e6f0fa31b183
Edgar Gomez <gomez@netseven.it>
2020-12-29 11:38:10 +0100

    updated api url

--- src/environments/environment.chrome-ext.ts
+++ src/environments/environment.chrome-ext.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   chromeExt: true,
   serializer: 'pundit-client',
-  baseURL: 'https://omaqn3375i.execute-api.eu-south-1.amazonaws.com/stage/'
+  baseURL: 'https://4b0gec68z3.execute-api.eu-south-1.amazonaws.com'
 };
 
 /*
