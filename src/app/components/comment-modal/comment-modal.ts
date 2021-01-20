7bf1200d6072a3f8839e1fd1e31e25b0de914adc
Edgar Gomez <gomez@netseven.it>
2021-01-20 18:11:15 +0100

    modal draggable

--- src/app/components/comment-modal/comment-modal.ts
+++ src/app/components/comment-modal/comment-modal.ts
@@ -1,7 +1,9 @@
 import {
+  AfterContentChecked,
   Component, Input
 } from '@angular/core';
 import { Icon } from '@n7-frontend/components';
+import * as Draggable from 'draggable';
 
 /**
  * Interface for CommentModal's "data"
@@ -42,10 +44,33 @@ export interface CommentModalData {
   selector: 'pnd-comment-modal',
   templateUrl: './comment-modal.html'
 })
-export class CommentModalComponent {
+export class CommentModalComponent implements AfterContentChecked {
   @Input() public data: CommentModalData;
 
   @Input() public emit: (type: string, payload?: any) => void;
+
+  private loaded = false;
+
+  public draggableTarget = 'pnd-modal-draggable-target';
+
+  public draggableHandle = 'pnd-modal-draggable-handle';
+
+  public draggableInstance;
+
+  ngAfterContentChecked() {
+    if (!this.loaded && this.data && this.data.visible) {
+      this.loaded = true;
+
+      // fix element dom loaded
+      setTimeout(() => {
+        const { shadowRoot } = document.getElementsByTagName('pnd-root')[0];
+        const target = shadowRoot.getElementById(this.draggableTarget);
+        const handle = shadowRoot.getElementById(this.draggableHandle);
+
+        this.draggableInstance = new Draggable(target, { handle });
+      });
+    }
+  }
 
   onClick(ev: Event, payload: any) {
     if (!this.emit) {
