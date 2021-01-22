49ffeef769b1a6cf5acf6388f0b12b8980dda6cd
Edgar Gomez <gomez@netseven.it>
2021-01-22 12:40:36 +0100

    updated build strategy

---
+++ scripts/postbuild.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs-extra');
+const concat = require('concat');
+
+const context = argv.c;
+const distPath = `${__dirname}/../dist`;
+const basePath = `${distPath}/${context}`;
+const filesToMerge = ['main.js', 'styles.js'];
+const outputFile = `pundit.${context}.js`;
+const outputFilePath = `${basePath}/${outputFile}`;
+let allowedFiles = [outputFile];
+
+if (argv.c === 'chrome-ext') {
+  allowedFiles = [
+    ...allowedFiles,
+    'assets',
+    'n7-icon',
+    'background.js',
+    'content.js',
+    'manifest.json',
+  ];
+}
+
+// merge in one file
+concat(filesToMerge.map((file) => `${basePath}/${file}`), outputFilePath)
+  .then(() => {
+    console.log(`Dist updated with ${outputFile}`);
+    return fs.readdir(basePath);
+  })
+  .then((files) => {
+    const filesToRemove = files.filter((file) => !allowedFiles.includes(file));
+
+    return Promise.all(
+      filesToRemove.map((file) => fs.remove(`${basePath}/${file}`))
+    );
+  })
+  .then(() => {
+    console.log(`Clean up unused files on ${basePath}`);
+  })
+  .catch((e) => {
+    console.error(`Build ${outputFile} error`, e);
+  });
--- src/chrome-ext/content.js
+++ src/chrome-ext/content.js
@@ -21,19 +21,12 @@ function load() {
   document.body.appendChild(appRoot);
   
   const main = document.createElement('script');
-  main.src = chrome.extension.getURL('main.js');
+  main.src = chrome.extension.getURL('pundit.chrome-ext.js');
   (document.head||document.documentElement).appendChild(main);
   main.onload = function() {
     // emit signal
     const signal = new CustomEvent("punditloaded", { detail: { id: chrome.runtime.id } });
     window.dispatchEvent(signal);
     main.remove();
-  };
-  
-  const styles = document.createElement('script');
-  styles.src = chrome.extension.getURL('styles.js');
-  (document.head||document.documentElement).appendChild(styles);
-  styles.onload = function() {
-      styles.remove();
   };
 }
