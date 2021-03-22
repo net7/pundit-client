9b7b2282cb8bac01429c340723fac19e70ab77ca
Edgar Gomez <gomez@netseven.it>
2021-03-22 10:46:24 +0100

    updated base url

--- src/environments/environment.embed.ts
+++ src/environments/environment.embed.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: false,
   chromeExt: false,
   serializer: 'pundit-client',
-  baseURL: 'https://4b0gec68z3.execute-api.eu-south-1.amazonaws.com',
+  baseURL: 'https://aw1qltfpxi.execute-api.eu-south-1.amazonaws.com/',
   ...local
 };
