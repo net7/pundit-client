6b651bb015a6a2635bba659bab8ea846e56343a1
Massimiliano Pardini <pardini@netseven.it>
2021-04-29 10:12:21 +0200

    script per deploy

---
+++ push-prod.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+rm -fr node_modules
+rm -fr package-lock.json
+#Update repo
+git pull origin $1
+#install dependencies
+npm install
+#build
+npm run build:embed-prod
+#push dist on s3
+export AWS_ACCESS_KEY_ID=$2; export AWS_SECRET_ACCESS_KEY=$3;
+mkdir dist/fonts;cd dist/fonts/; git clone git@github.com:net7/pundit-icon-font.git
+aws s3 sync --acl public-read  --region='eu-south-1' pundit-icon-font s3://static-pundit/pundit-icon-font
+cd ../..;
+aws s3 sync --acl public-read  --region='eu-south-1' dist/embed-prod s3://static-pundit/releases/
+rm -fr dist/fonts
+aws cloudfront create-invalidation --distribution-id E3JMMV8C3EZFVD --paths "/*"
