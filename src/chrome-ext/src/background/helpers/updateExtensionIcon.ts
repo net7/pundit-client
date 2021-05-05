75e5c87a469ea7ea98b7656be01dde2560dfe92c
Edgar Gomez <gomez@netseven.it>
2021-05-05 15:44:04 +0200

    updated icons build

--- src/chrome-ext/src/background/helpers/updateExtensionIcon.ts
+++ src/chrome-ext/src/background/helpers/updateExtensionIcon.ts
@@ -1,7 +1,7 @@
 import { updateBadgeText } from './updateBadgeText';
 import { updateBadgeTitle } from './updateBadgeTitle';
 
-const ICON_ON = 'assets/icons/pundit-icon-red-38.png';
+const ICON_ON = 'assets/icons/pundit-icon-38-close.png';
 const ICON_OFF = 'assets/icons/pundit-icon-38.png';
 const BADGE_COLOR_ON: chrome.browserAction.ColorArray = [13, 133, 236, 255];
 const BADGE_COLOR_OFF: chrome.browserAction.ColorArray = [128, 128, 128, 255];
