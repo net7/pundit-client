923df5776420d4ccaaef1657c630b2cb2fe8da24
Edgar Gomez <gomez@netseven.it>
2021-10-27 10:25:15 +0200

    fix local config type check

--- src/environments/environment.chrome-ext.prod.ts
+++ src/environments/environment.chrome-ext.prod.ts
@@ -1,8 +1,11 @@
+import { LocalConfig } from './configuration-types';
 import local from './local.prod';
+
+const localConfig: LocalConfig = local;
 
 export const environment = {
   production: true,
   chromeExt: true,
   showLoginToast: true,
-  ...local
+  ...localConfig
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
+import { LocalConfig } from './configuration-types';
 import local from './local.stage';
+
+const localConfig: LocalConfig = local;
 
 export const environment = {
   production: false,
   chromeExt: false,
   showLoginToast: true,
-  ...local,
+  ...localConfig,
 };
