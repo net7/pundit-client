04d1d7164aa168050140a872fe64e54f038459ad
Luca <apoggianti.luca@gmail.com>
2023-12-05 12:01:42 +0100

    NotebookShareUserSelectedComponent updated

--- src/app/components/notebook-share-user-selected/notebook-share-user-selected.ts
+++ src/app/components/notebook-share-user-selected/notebook-share-user-selected.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ImageDataService } from 'src/app/services/image-data.service';
+import { NotebookShareModalData, NotebookShareModalResult } from '../notebook-share-modal/notebook-share-modal';
 
 @Component({
   selector: 'pnd-notebook-share-user-selected',
   templateUrl: './notebook-share-user-selected.html'
 })
 export class NotebookShareUserSelectedComponent implements OnInit {
-  constructor() {
-    //
-  }
+  @Input() data: NotebookShareModalData;
+
+  actions = [
+    { label: 'Can read', value: 'read' },
+    { label: 'Can write', value: 'write' }
+  ];
+
+  item: NotebookShareModalResult;
+
+  dropdownExpanded = false;
+
+  statusLabel = this.actions[0].label;
+
+  constructor(
+    public imageDataService: ImageDataService
+  ) {}
 
   ngOnInit(): void {
-    //
+    this.item = this.data.body.confirmSection.selected;
+    this.item.action = 'read';
+  }
+
+  dropdownToggle() {
+    this.dropdownExpanded = !this.dropdownExpanded;
+  }
+
+  onActionClick(payload) {
+    this.statusLabel = payload.label;
+    this.item.action = payload.value;
+    this.dropdownExpanded = !this.dropdownExpanded;
   }
 }
