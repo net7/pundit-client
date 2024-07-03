bfa2bfadf673b166c914fa264a2e8d8e07003093
Marco Zizi <zizi@netseven.it>
2024-07-03 11:45:35 +0200

    Fix webpack build stage

--- webpack.chrome-ext.stage.js
+++ webpack.chrome-ext.stage.js
@@ -7,8 +7,8 @@ module.exports = prodConfig.map((config) => ({
   plugins: [
     new webpack.DefinePlugin({
       API_BASE_URL: JSON.stringify('https://api.pundithomex.netseven.work'),
-      FEED_PDF_BASE_URL: JSON.stringify('http://feedthepund.max/pdf.php?source='),
-      FEED_WEB_BASE_URL: JSON.stringify('http://feedthepund.max/?url='),
+      FEED_PDF_BASE_URL: JSON.stringify('http://feed.netseven.work/pdf.php?source='),
+      FEED_WEB_BASE_URL: JSON.stringify('http://feed.netseven.work/?url='),
     })
   ]
 }));
