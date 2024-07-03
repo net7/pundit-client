c70d670a511d8bfc08079b0647f6e6ab2b94e46a
Marco Zizi <zizi@netseven.it>
2024-07-03 11:07:05 +0200

    Fix ext background

--- src/chrome-ext/src/background/helpers/checkActiveState.ts
+++ src/chrome-ext/src/background/helpers/checkActiveState.ts
@@ -9,7 +9,7 @@ import {
   updateExtensionIcon,
   getFeedPdfSource,
   getFeedWebSource,
-  isFeedWebUrl
+  isFeedWebUrl,
 } from '.';
 import { CommonEventType } from '../../../../common/types';
 import { onBrowserActionClicked } from '../handlers/onBrowserActionClicked';
@@ -26,66 +26,66 @@ export const checkActiveState = (tabId: number) => {
           return;
         }
         const activeKey = `${ChromeExtStorageKey.Active}.${tabId}`;
-        ChromeExtStorage.get(activeKey)
-          .then((active: boolean) => {
-            const {
-              url: tabUrl,
-              // status: tabStatus
-            } = tab;
-            fetch(tabUrl).then((response) => {
-              const contentType = response.headers.get('Content-Type');
-              const isPdf = isPdfDocument(tabUrl, contentType);
-              const isViewer = isPdfViewer(tabUrl);
-              const isFeedWeb = isFeedWebUrl(tabUrl);
-              const isFeedPdf = isFeedPdfUrl(tabUrl);
-              let skipIconUpdate = false;
-              if (isFeedWeb) {
-                const webDocumentSource = getFeedWebSource(tabUrl);
-                // redirect
-                chrome.tabs.update({
-                  url: decodeURIComponent(webDocumentSource)
-                });
-                // trigger click
-                if (!active) {
-                  setTimeout(() => {
-                    onBrowserActionClicked(tab);
-                  });
-                }
-              } else if (isPdf && isFeedPdf) {
-                const pdfSource = getFeedPdfSource(tabUrl);
-                // redirect
-                redirectToPdfViewer(pdfSource);
-                if (!active) {
-                  skipIconUpdate = true;
-                  setTimeout(() => {
-                    onBrowserActionClicked(tab, true);
-                    updateExtensionIcon(tab.id, true);
-                  });
-                }
-              } else if (active && isPdf) {
-                redirectToPdfViewer(tabUrl);
-              } else if (!active && isViewer) {
-                redirectToOriginalPdfUrl(tabUrl);
-              } else {
-                // if inactive get document/page url
-                // total annotations number
-                const payload = { active };
-                // if (tabStatus === 'complete') {
-                //   chrome.tabs.sendMessage(tabId, {
-                //     payload,
-                //     type: CommonEventType.DocumentInfoRequest,
-                //   });
-                // }
-                chrome.tabs.sendMessage(tabId, {
-                  payload,
-                  type: CommonEventType.StateChanged,
-                });
-              }
-              if (!skipIconUpdate) {
-                updateExtensionIcon(tabId, active);
-              }
+        ChromeExtStorage.get(activeKey).then(async (active: boolean) => {
+          const {
+            url: tabUrl,
+            // status: tabStatus
+          } = tab;
+
+          const isViewer = isPdfViewer(tabUrl);
+          const isFeedWeb = isFeedWebUrl(tabUrl);
+          const isFeedPdf = isFeedPdfUrl(tabUrl);
+          let skipIconUpdate = false;
+          if (isFeedWeb) {
+            const webDocumentSource = getFeedWebSource(tabUrl);
+            // redirect
+            chrome.tabs.update({
+              url: decodeURIComponent(webDocumentSource),
             });
-          });
+            // trigger click
+            if (!active) {
+              setTimeout(() => {
+                onBrowserActionClicked(tab);
+              });
+            }
+            return;
+          }
+
+          const isPdf = await isPdfDocument(tabUrl);
+          if (isPdf && isFeedPdf) {
+            const pdfSource = getFeedPdfSource(tabUrl);
+            // redirect
+            redirectToPdfViewer(pdfSource);
+            if (!active) {
+              skipIconUpdate = true;
+              setTimeout(() => {
+                onBrowserActionClicked(tab, true);
+                updateExtensionIcon(tab.id, true);
+              });
+            }
+          } else if (active && isPdf) {
+            redirectToPdfViewer(tabUrl);
+          } else if (!active && isViewer) {
+            redirectToOriginalPdfUrl(tabUrl);
+          } else {
+            // if inactive get document/page url
+            // total annotations number
+            const payload = { active };
+            // if (tabStatus === 'complete') {
+            //   chrome.tabs.sendMessage(tabId, {
+            //     payload,
+            //     type: CommonEventType.DocumentInfoRequest,
+            //   });
+            // }
+            chrome.tabs.sendMessage(tabId, {
+              payload,
+              type: CommonEventType.StateChanged,
+            });
+          }
+          if (!skipIconUpdate) {
+            updateExtensionIcon(tabId, active);
+          }
+        });
       });
     }
   });
--- src/chrome-ext/src/background/helpers/isPdfDocument.ts
+++ src/chrome-ext/src/background/helpers/isPdfDocument.ts
@@ -1,7 +1,21 @@
 import { getPdfViewerPath } from '.';
 
-export const isPdfDocument = (url: string, contentType: string) => (
-  typeof url === 'string'
-  && (url.includes('.pdf') || contentType === 'application/pdf')
-  && !url.startsWith(getPdfViewerPath())
-);
+export const isPdfDocument = async (url: string) => {
+  if (typeof url !== 'string') {
+    return false;
+  }
+  const includePdf = url.includes('.pdf');
+  const isNotViewerPath = !url.startsWith(getPdfViewerPath());
+  if (isNotViewerPath && includePdf) {
+    return true;
+  }
+  if (isNotViewerPath && !includePdf) {
+    try {
+      const response = await fetch(url);
+      return response.headers.get('Content-Type') === 'application/pdf';
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return false;
+};
